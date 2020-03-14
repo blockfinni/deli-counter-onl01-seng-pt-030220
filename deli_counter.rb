71adc72d8e6bccfc815fa6bbcaa8aeeeb87fa85b
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-14 01:39:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array) # this was the one I figured out
       array1.push("#{counter}. #{customer}")
       counter += 1 
     end 
-    puts "The line is currently: #{nuarray.join(" ")}"
+    puts "The line is currently: #{array1.join(" ")}"
   else
     puts "The line is empty."
   end
