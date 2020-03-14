ad1f7676b9623fb5afbff72f6d3ca8e7a648a14d
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-14 01:38:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,14 @@ def line(array) # this was the one I figured out
     puts "The line is empty."
   end
 end
+
+def line_simple(array)
+  current_line = "The simple line is currently:"
+  
+  array.each.with_index(1) do |value, indexemus|  
+  
+    current_line << " #{indexemus}. #{value},"    
+   
+  end 
+  puts current_line
+end 
