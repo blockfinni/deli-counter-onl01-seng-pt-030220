566dd9e23bfd31c5895033e59b89a5a55ce6626e
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-14 01:41:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(array) # this was the one I figured out
   if array.length >= 1
     array1 = []
     counter = 1 
-    array.each do |name|
-      array1.push("#{counter}. #{name}")
+    array.each do |customer|
+      array1.push("#{counter}. #{customer}")
       counter += 1 
     end 
     puts "The line is currently: #{array1.join(" ")}"
